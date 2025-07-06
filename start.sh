9c2942a6fd9425eeb150a4d2ffaf9a4f0bc49e00
ram0420 <yeram0420@ewhain.net>
2025-07-06 13:46:33 +0900

    0706_6

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# uvicorn app.main:app --host=0.0.0.0 --port=10000
+uvicorn app.main:app --host=0.0.0.0 --port=10000
 
-#!/bin/bash
-uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-8000}
+# #!/bin/bash
+# uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-8000}
